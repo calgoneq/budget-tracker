ab520213ed32ce434c30e1013e8ccd6f6d71d4a0
calgoneq <calgoneq@gmail.com>
2026-05-22 15:48:57 +0200

    feat: add GET /transactions/summary endpoint

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from exceptions import ValidationError
 from transaction import Transaction
 from models import TransactionIn
 from db import init_db, get_all_transactions, get_transaction_by_id, add_transaction, remove_transaction
+from ai import generate_summary
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()
+    await init_db()
     yield
 
 app = FastAPI(lifespan=lifespan)
@@ -19,12 +20,22 @@ async def get_status():
 
 @app.get("/transactions")
 async def get_transactions(kategoria: str = None):
-    transactions = get_all_transactions(kategoria)
+    transactions = await get_all_transactions(kategoria)
     return transactions
+
+@app.get("/transactions/summary")
+async def get_summary():
+    transaction = await get_all_transactions(None)
+    try:
+        ai_response = await generate_summary(transaction)
+        return ai_response
+    except Exception as e:
+        print(f"ERROR: {e}")
+        raise HTTPException(status_code=503, detail=str(e))
 
 @app.get("/transactions/{transaction_id}")
 async def get_transactions_by_id(transaction_id: int):
-    transaction = get_transaction_by_id(transaction_id)
+    transaction = await get_transaction_by_id(transaction_id)
     if transaction is None:
         raise HTTPException(status_code=404, detail=f"Transakcja o id {transaction_id} nie istnieje")
 
@@ -35,14 +46,14 @@ async def post_transaction(item: TransactionIn):
     try:
         transaction = Transaction(item.sklep, item.kwota, item.kategoria, item.data)
         data = transaction.to_dict()
-        response = add_transaction(data)
+        response = await add_transaction(data)
         return {"message": "ok", "transaction": response}
     except ValidationError as e:
         raise HTTPException(status_code=422, detail=str(e))
 
 @app.delete("/transactions/{transaction_id}")
 async def delete_transactions(transaction_id: int):
-    if remove_transaction(transaction_id):
+    if await remove_transaction(transaction_id):
         return {"message": f"Usunięto transakcje o id {transaction_id}"}
     else:
         raise HTTPException(status_code=404, detail=f"Transakcja o id {transaction_id} nie istnieje")
